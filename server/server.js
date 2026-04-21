89b5e257e73d162be087437a4b1abd09b817e74e
KanikaSamant05 <kanika7260@gmail.com>
2026-04-21 14:56:33 +0530

    render error resolved

--- server/server.js
+++ server/server.js
@@ -18,12 +18,15 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
 
-app.use(cors());
+app.use(cors({
+origin: process.env.CLIENT_URL
+}));
+
 app.use(express.json());
 app.set("io", io);
 
